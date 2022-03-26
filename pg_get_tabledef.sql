e8ec2589a17ffce27feb3ecf9dadb172740749b9
Michael Vitale <MichaelDBA@users.noreply.github.com>
2022-03-26 13:14:44 -0400

    pg_get_tabledef.sql

--- pg_get_tabledef.sql
+++ pg_get_tabledef.sql
@@ -1,8 +1,31 @@
+do $$ 
+<<first_block>>
+DECLARE
+    cnt int;
+BEGIN
+  SELECT count(*) into cnt
+  FROM pg_catalog.pg_type t LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid)) 
+  AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)
+  AND n.nspname <> 'pg_catalog' AND n.nspname <> 'information_schema' AND pg_catalog.pg_type_is_visible(t.oid)
+  AND pg_catalog.format_type(t.oid, NULL) in ('tabledef_fkeys','tabledef_trigs');
+  IF cnt = 0 THEN
+    RAISE INFO 'Creating custom types.';
+    CREATE TYPE public.tabledef_fkeys AS ENUM ('FKEYS_INTERNAL', 'FKEYS_EXTERNAL', 'FKEYS_COMMENTED', 'FKEYS_NONE');
+    CREATE TYPE public.tabledef_trigs AS ENUM ('INCLUDE_TRIGGERS', 'NO_TRIGGERS');
+  END IF;
+end first_block $$;
+
+-- SELECT count(*) into cnt
+-- FROM pg_catalog.pg_type t LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid)) 
+-- AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)
+-- AND n.nspname <> 'pg_catalog' AND n.nspname <> 'information_schema' AND pg_catalog.pg_type_is_visible(t.oid)
+-- AND pg_catalog.format_type(t.oid, NULL) in ('tabledef_fkeys','tabledef_trigs');
+
 -- Create enum types used by this function
-DROP TYPE IF EXISTS public.tabledef_fkeys;
-DROP TYPE IF EXISTS public.tabledef_trigs;
-CREATE TYPE public.tabledef_fkeys AS ENUM ('FKEYS_INTERNAL', 'FKEYS_EXTERNAL', 'FKEYS_COMMENTED', 'FKEYS_NONE');
-CREATE TYPE public.tabledef_trigs AS ENUM ('INCLUDE_TRIGGERS', 'NO_TRIGGERS');
+-- DROP TYPE IF EXISTS public.tabledef_fkeys;
+-- DROP TYPE IF EXISTS public.tabledef_trigs;
+-- CREATE TYPE public.tabledef_fkeys AS ENUM ('FKEYS_INTERNAL', 'FKEYS_EXTERNAL', 'FKEYS_COMMENTED', 'FKEYS_NONE');
+-- CREATE TYPE public.tabledef_trigs AS ENUM ('INCLUDE_TRIGGERS', 'NO_TRIGGERS');
 
 -- SELECT * FROM public.pg_get_tabledef('sample', 'address');
 CREATE OR REPLACE FUNCTION public.pg_get_tabledef(
