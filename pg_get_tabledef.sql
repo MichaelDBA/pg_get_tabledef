8ffdc6c67cc14a5fd089acf8937a8c43e3b6ba25
Michael Vitale <MichaelDBA@users.noreply.github.com>
2024-12-26 09:39:43 -0500

    pg_get_tabledef.sql

--- pg_get_tabledef.sql
+++ pg_get_tabledef.sql
@@ -1,7 +1,8 @@
 /* ********************************************************************************
 COPYRIGHT NOTICE FOLLOWS.  DO NOT REMOVE
-Copyright (c) 2021-2024 SQLEXEC LLC
+Copyright (c) 2021-2025 SQLEXEC LLC
 
+			GNU General Public License v3.0
 Permission to use, copy, modify, and distribute this software and its documentation 
 for any purpose, without fee, and without a written agreement is hereby granted, 
 provided that the above copyright notice and this paragraph and the following two paragraphs appear in all copies.
@@ -158,7 +159,7 @@ LANGUAGE plpgsql VOLATILE
 AS
 $$
   DECLARE
-    v_version        text := '2.3 December 15, 2024';
+    v_version        text := '2.3 December 15, 2024  GNU License 3.0'
     v_schema    text := '';
     v_coldef    text := '';
     v_qualified text := '';
